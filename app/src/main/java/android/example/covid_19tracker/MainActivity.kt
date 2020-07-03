de03b50332c936510f923ffa392ac4db8bf2398d
akanksha606 <akankshagupta98583@gmail.com>
2020-07-03 22:20:40 +0530

    Final changes

--- app/src/main/java/android/example/covid_19tracker/MainActivity.kt
+++ app/src/main/java/android/example/covid_19tracker/MainActivity.kt
@@ -1,9 +1,15 @@
+@file:Suppress("DEPRECATION")
+
 package android.example.covid_19tracker
 
+import android.content.Context
+import android.net.ConnectivityManager
+import android.net.NetworkInfo
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
+import android.widget.Toast
 import androidx.core.content.ContextCompat
 import androidx.fragment.app.FragmentTransaction
 import com.ismaeldivita.chipnavigation.ChipNavigationBar
@@ -15,31 +21,51 @@ class MainActivity : AppCompatActivity() {
     lateinit var fragmentSymptom: fragment_symptom
     lateinit var fragmentHome: fragment_home
     lateinit var fragmentPrecaution: fragment_precaution
+    lateinit var fragmentInternet: fragment_internet
+    lateinit var fragmentStatewise: fragment_statewise
+    private fun isNetworkAvailable(): Boolean {
+        val connectivityManager = getSystemService(Context.CONNECTIVITY_SERVICE)
+        return if (connectivityManager is ConnectivityManager) {
+            val networkInfo: NetworkInfo? = connectivityManager.activeNetworkInfo
+            networkInfo?.isConnected ?: false
+        } else false
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         supportActionBar?.hide()
       val chipNavigationBar:ChipNavigationBar=findViewById(R.id.menu_bottom)
         chipNavigationBar.setItemSelected(R.id.home,true)
-        fragmentHome= fragment_home()
-        supportFragmentManager
-            .beginTransaction()
-            .replace(R.id.fragment_container,fragmentHome)
-            .setTransition(FragmentTransaction.TRANSIT_FRAGMENT_OPEN)
-            .commit()
+        if (isNetworkAvailable()) {
+            fragmentHome = fragment_home()
+            supportFragmentManager
+                .beginTransaction()
+                .replace(R.id.fragment_container, fragmentHome)
+                .setTransition(FragmentTransaction.TRANSIT_FRAGMENT_OPEN)
+                .commit()
+
+
 
 
         chipNavigationBar.setOnItemSelectedListener { id->
             when(id){
                R.id.home->{
-                   fragmentHome= fragment_home()
-                   supportFragmentManager
-                       .beginTransaction()
-                       .replace(R.id.fragment_container,fragmentHome)
-                       .setTransition(FragmentTransaction.TRANSIT_FRAGMENT_OPEN)
-                       .commit()
-
+                   fragmentHome = fragment_home()
+                       supportFragmentManager
+                           .beginTransaction()
+                           .replace(R.id.fragment_container, fragmentHome)
+                           .setTransition(FragmentTransaction.TRANSIT_FRAGMENT_OPEN)
+                           .commit()
                }
+                R.id.statewise->{
+                    fragmentStatewise = fragment_statewise()
+                    supportFragmentManager
+                        .beginTransaction()
+                        .replace(R.id.fragment_container, fragmentStatewise)
+                        .setTransition(FragmentTransaction.TRANSIT_FRAGMENT_OPEN)
+                        .commit()
+                }
                 R.id.precaution->{
                     fragmentPrecaution= fragment_precaution()
                     supportFragmentManager
@@ -70,4 +96,66 @@ class MainActivity : AppCompatActivity() {
             }
         }
         true
-    }}
+        }
+        else{
+            fragmentInternet = fragment_internet()
+            supportFragmentManager
+                .beginTransaction()
+                .replace(R.id.fragment_container, fragmentInternet)
+                .setTransition(FragmentTransaction.TRANSIT_FRAGMENT_OPEN)
+                .commit()
+
+            //Toast.makeText(this,"No net Connection",Toast.LENGTH_LONG).show()
+        chipNavigationBar.setOnItemSelectedListener { id ->
+            when (id) {
+                R.id.home->{
+
+                        fragmentInternet = fragment_internet()
+                        supportFragmentManager
+                            .beginTransaction()
+                            .replace(R.id.fragment_container, fragmentInternet)
+                            .setTransition(FragmentTransaction.TRANSIT_FRAGMENT_OPEN)
+                            .commit()
+
+                }
+                R.id.statewise->{
+
+                        fragmentInternet = fragment_internet()
+                        supportFragmentManager
+                            .beginTransaction()
+                            .replace(R.id.fragment_container, fragmentInternet)
+                            .setTransition(FragmentTransaction.TRANSIT_FRAGMENT_OPEN)
+                            .commit()
+
+                }
+                R.id.precaution -> {
+                    fragmentPrecaution = fragment_precaution()
+                    supportFragmentManager
+                        .beginTransaction()
+                        .replace(R.id.fragment_container, fragmentPrecaution)
+                        .setTransition(FragmentTransaction.TRANSIT_FRAGMENT_OPEN)
+                        .commit()
+
+                }
+                R.id.symptom -> {
+                    fragmentSymptom = fragment_symptom()
+                    supportFragmentManager
+                        .beginTransaction()
+                        .replace(R.id.fragment_container, fragmentSymptom)
+                        .setTransition(FragmentTransaction.TRANSIT_FRAGMENT_OPEN)
+                        .commit()
+
+                }
+                R.id.about -> {
+                    fragmentAbout = fragment_about()
+                    supportFragmentManager
+                        .beginTransaction()
+                        .replace(R.id.fragment_container, fragmentAbout)
+                        .setTransition(FragmentTransaction.TRANSIT_FRAGMENT_OPEN)
+                        .commit()
+
+                }
+            }
+        } }
+    }
+}
--- app/src/main/java/android/example/covid_19tracker/fragment_home.kt
+++ app/src/main/java/android/example/covid_19tracker/fragment_home.kt
@@ -1,14 +1,22 @@
+@file:Suppress("DEPRECATION")
 package android.example.covid_19tracker
 
 import android.content.Context
+import android.graphics.Color
 import android.net.ConnectivityManager
 import android.net.NetworkInfo
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
 import androidx.core.content.ContextCompat.getSystemService
 import androidx.fragment.app.Fragment
+import com.github.mikephil.charting.animation.Easing
+import com.github.mikephil.charting.data.PieData
+import com.github.mikephil.charting.data.PieDataSet
+import com.github.mikephil.charting.data.PieEntry
+import com.github.mikephil.charting.formatter.PercentFormatter
 import com.google.gson.Gson
 import kotlinx.android.synthetic.main.fragment_home.*
 import kotlinx.coroutines.Dispatchers
@@ -21,41 +29,92 @@ import java.util.concurrent.TimeUnit
 
 
 class fragment_home : Fragment() {
-    lateinit var stateAdapter: StateAdapter
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
+
+
     ): View? {
         // Inflate the layout for this fragment
-
-            return inflater.inflate(R.layout.fragment_home, container, false)
+        return inflater.inflate(R.layout.fragment_home, container, false)
 
     }
 
     override fun onActivityCreated(savedInstanceState: Bundle?) {
         super.onActivityCreated(savedInstanceState)
         fetchResults()
+        refreshSwipe.setColorSchemeResources(R.color.home_color)
+        refreshSwipe.setOnRefreshListener {
+            if(isNetworkAvailable())
+            {
+                spin_kit.visibility=View.GONE
+                titleHomeNew.visibility=View.VISIBLE
+                home_container.visibility=View.GONE
+                no_internet.visibility=View.GONE
+
+                GlobalScope.launch {
+                    val response= withContext(Dispatchers.IO){
+
+                        Client.api.clone().execute() }
+
+                    if(response.isSuccessful){
+                        val data= Gson().fromJson(response.body?.string(),Response::class.java)
+                        launch(Dispatchers.Main) {
+                            refreshSwipe.isRefreshing=false
+                            bindCombinedData(data.statewise[0])
+                            titleHomeNew.visibility=View.GONE
+                            home_container.visibility=View.VISIBLE
+
+                        }
+                    }
+                }
+
+            }
+            else
+            {   refreshSwipe.isRefreshing=false
+                no_internet.visibility=View.VISIBLE
+                titleHomeNew.visibility=View.VISIBLE
+                home_container.visibility=View.GONE
+            }
+        }
+
     }
+
+
+
 
     private fun fetchResults() {
-    GlobalScope.launch {
-     val response= withContext(Dispatchers.IO){Client.api.clone().execute() }
-     if(response.isSuccessful){
-     val data= Gson().fromJson(response.body?.string(),Response::class.java)
-         launch(Dispatchers.Main) {
-             bindCombinedData(data.statewise[0])
-             bindStateWiseData(data.statewise.subList(1,data.statewise.size))
-         }
-     }
-    }
+        spin_kit.visibility=View.VISIBLE
+        titleHomeNew.visibility=View.VISIBLE
+        home_container.visibility=View.GONE
+        if(isNetworkAvailable())
+        {
+            GlobalScope.launch {
+                val response= withContext(Dispatchers.IO){
+
+                    Client.api.clone().execute() }
+
+                if(response.isSuccessful) {
+                    val data = Gson().fromJson(response.body?.string(), Response::class.java)
+                    launch(Dispatchers.Main) {
+                        bindCombinedData(data.statewise[0])
+                        spin_kit.visibility = View.GONE
+                        titleHomeNew.visibility = View.GONE
+                        home_container.visibility = View.VISIBLE
+
+                    }
+                }
+            }
+        }
+        else
+        {
+            no_internet.visibility=View.VISIBLE
+            home_container.visibility=View.GONE
+            titleHomeNew.visibility=View.VISIBLE
+        }
     }
 
-    private fun bindStateWiseData(subList: List<StatewiseItem>) {
-        stateAdapter=StateAdapter(subList)
-        list.adapter=stateAdapter
-
-    }
 
     private fun bindCombinedData(data: StatewiseItem) {
      val lastUpdatedTime=data.lastupdatedtime
@@ -65,6 +124,47 @@ class fragment_home : Fragment() {
         activeTv.text=data.active
         recoveredTv.text=data.recovered
         deceasedTv.text=data.deaths
+
+        val yVals = ArrayList<PieEntry>()
+        yVals.add(PieEntry((data.confirmed)!!.toFloat()))
+        yVals.add(PieEntry((data.active)!!.toFloat()))
+        yVals.add(PieEntry((data.recovered)!!.toFloat()))
+        yVals.add(PieEntry((data.deaths)!!.toFloat()))
+
+        val dataSet = PieDataSet(yVals, "")
+        dataSet.valueTextSize=0f
+        dataSet.sliceSpace=2f
+        val colors = ArrayList<Int>()
+        colors.add(Color.parseColor("#d84349"))
+        colors.add(Color.parseColor("#31a7fb"))
+        colors.add(Color.parseColor("#00cb75"))
+        colors.add(Color.parseColor("#ffc250"))
+        try {
+            val activePercentage = (data.active)!!.toDouble() / (data.confirmed)!!.toDouble() * 100
+            val recoveredPercentage = (data.recovered)!!.toDouble() / (data.confirmed)!!.toDouble() * 100
+            val deathsPercentage = (data.deaths)!!.toDouble() / (data.confirmed)!!.toDouble() * 100
+
+            active_percentage.text = String.format("%.2f", activePercentage).plus("%")
+            recovered_percentage.text = String.format("%.2f", recoveredPercentage).plus("%")
+            death_percentage.text = String.format("%.2f", deathsPercentage).plus("%")
+            statusLayout.visibility = View.VISIBLE
+        } catch (e: Exception) {
+            e.printStackTrace()
+        }
+
+        dataSet.setColors(colors)
+        val data = PieData(dataSet)
+        pieChart.data = data
+        pieChart.centerTextRadiusPercent = 0f
+        pieChart.isDrawHoleEnabled = true
+        pieChart.legend.isEnabled = false
+        pieChart.description.isEnabled = false
+        pieChart.isRotationEnabled=true
+        pieChart.setUsePercentValues(true)
+        pieChart.setDrawRoundedSlices(true)
+        pieChart.transparentCircleRadius=60f
+        pieChart.holeRadius=68f
+        pieChart.animateY(1400, Easing.EaseInOutQuad)
 
     }
 
@@ -89,6 +189,14 @@ class fragment_home : Fragment() {
             }
         }
 
+    }
+    private fun isNetworkAvailable(): Boolean {
+        val connectivityManager = activity!!.getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager
+        return if (connectivityManager is ConnectivityManager) {
+            val networkInfo: NetworkInfo? = connectivityManager.activeNetworkInfo
+            networkInfo?.isConnected ?: false
+
+        } else false
     }
 
 }
